7a40994c0cbf12a91e6106c40ccf353914f2ff93
Ahmed Samir <ahmed.samir6446@gmail.com>
2020-09-03 13:42:31 +0200

    ddate formatter

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: 'AIzaSyCBj1S76VfRt7QB46fX6vS80l3ngvcdx9Q',
+    authDomain: 'quality-realstate.firebaseapp.com',
+    databaseURL: 'https://quality-realstate.firebaseio.com',
+    projectId: 'quality-realstate',
+    storageBucket: 'quality-realstate.appspot.com',
+    messagingSenderId: '654600750741',
+    appId: '1:654600750741:web:66098d2f6a4a9997597d9c',
+    measurementId: 'G-MX1L51GV72'
+  }
 };
